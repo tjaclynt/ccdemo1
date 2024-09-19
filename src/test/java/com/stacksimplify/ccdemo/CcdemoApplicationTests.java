071e61ca7f30e63428c1f6f7238f405eb3b0fac8
Jaclyn <tjaclynt@gmail.com>
2024-09-19 10:50:26 -0400

    Add files via upload

---
+++ src/test/java/com/stacksimplify/ccdemo/CcdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.ccdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CcdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
